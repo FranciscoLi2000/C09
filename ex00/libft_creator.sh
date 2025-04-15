f6bcb201690811301ee2eb8fa442b710ebb8b451
FranciscoLi2000 <liyufeng577@gmail.com>
2025-04-15 15:45:53 +0000

    mejoras

--- ex00/libft_creator.sh
+++ ex00/libft_creator.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # Compile all source files
-cc -Wall -Wextra -Werror -Iincludes -c srcs/ft_putchar.c
-cc -Wall -Wextra -Werror -Iincludes -c srcs/ft_swap.c
-cc -Wall -Wextra -Werror -Iincludes -c srcs/ft_putstr.c
-cc -Wall -Wextra -Werror -Iincludes -c srcs/ft_strlen.c
-cc -Wall -Wextra -Werror -Iincludes -c srcs/ft_strcmp.c
+cc -Wall -Wextra -Werror -Iincludes -c ft_putchar.c
+cc -Wall -Wextra -Werror -Iincludes -c ft_swap.c
+cc -Wall -Wextra -Werror -Iincludes -c ft_putstr.c
+cc -Wall -Wextra -Werror -Iincludes -c ft_strlen.c
+cc -Wall -Wextra -Werror -Iincludes -c ft_strcmp.c
 
 # Create library
 ar rcs libft.a ft_putchar.o ft_swap.o ft_putstr.o ft_strlen.o ft_strcmp.o
